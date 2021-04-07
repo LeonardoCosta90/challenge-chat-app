60ee253a88206c23a69fcb4b7b7ea28cb11e1b5d
Leonardo Costa <leonardoeverton.tec@gmail.com>
2021-04-07 18:29:27 -0300

    feat: create component Chat Join

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Join from './components/Join';
+import Chat from './components/Chat';
+
 const App = () => (
   <Router>
-    <Route path="/" exact component={join} />
-    <Route path="/chat" exact component={join} />
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" exact component={Chat} />
   </Router>
 );
 
