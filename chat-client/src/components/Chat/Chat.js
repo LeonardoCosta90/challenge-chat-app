6850f9c3deabed4e64cdd437f5627c74e04005f5
Leonardo Costa <leonardoeverton.tec@gmail.com>
2021-04-08 18:43:00 -0300

    feat: conenction user chat

--- chat-client/src/components/Chat/Chat.js
+++ chat-client/src/components/Chat/Chat.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+const ENDPOINT = 'localhost:5000';
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    var connectionOptions = {
+      'force new connection': true,
+      reconnectionAttempts: 'Infinity',
+      timeout: 10000,
+      transports: ['websocket'],
+    };
+
+    socket = io.connect('http://localhost:5000', connectionOptions);
+    //socket = io(ENDPOINT);
+
+    //setRoom(room);
+    //setName(name);
+
+    socket.emit('join', { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+
   return <h1>Chat</h1>;
 };
 
--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
-
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
 
 const app = express();
+
 const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connect', (socket) => {
   console.log('We have a new connection!!!');
+
+  socket.on('join', ({ name, room }) => {
+    console.log(name, room);
+  });
 
   socket.on('disconnect', () => {
     console.log('User had left!!');
