455dbd46baf42abe4e2e5bc65e69790421401390
Leonardo Costa <leonardoeverton.tec@gmail.com>
2021-04-11 15:38:44 -0300

    feat: using messages room and TextContainer

--- server/index.js
+++ server/index.js
@@ -19,16 +19,20 @@ io.on('connect', (socket) => {
 
     if (error) return callback(error);
 
+    socket.join(user.room);
+
     socket.emit('message', {
       user: 'admin',
-      text: `${user.name}, welcome to the room ${user.room}`,
+      text: `${user.name}, welcome to room ${user.room}.`,
     });
-
     socket.broadcast
       .to(user.room)
-      .emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+      .emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
-    socket.join(user.room);
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
     callback();
   });
@@ -42,9 +46,21 @@ io.on('connect', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('User had left!!');
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'Admin',
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
   });
 });
-app.use(router);
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(process.env.PORT || 5000, () =>
+  console.log(`Server has started.`)
+);
